665ab9a4a20eb5ef47b275558f92d5378597172e
oliverloops <oliveralr@protonmail.com>
2022-12-29 10:56:04 -0600

    Initial Commit

---
+++ Neotify/NeotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeotifyApp.swift
+//  Neotify
+//
+//  Created by Oliver Lopez on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NeotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
